dcb016224ec34252b9bd1f23138343f3184098c0
CaioAzs <souzascaio23cs@gmail.com>
2023-03-17 10:53:02 -0300

    Added Cross Origin to Client Controller

--- Omicron-API/src/main/java/com/caioazs/omicron/controllers/ClientController.java
+++ Omicron-API/src/main/java/com/caioazs/omicron/controllers/ClientController.java
@@ -2,6 +2,7 @@ package com.caioazs.omicron.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/clients")
+@CrossOrigin("http://localhost:4200")
 public class ClientController {
 
     @Autowired
